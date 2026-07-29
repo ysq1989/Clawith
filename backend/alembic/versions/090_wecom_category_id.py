c2a2067e6bf958cbfa80eb0128d1b4a85b3a5b0e
ysq1989 <100890610@qq.com>
2026-07-29 19:25:11 +0800

    fix: correct migration revision IDs

--- backend/alembic/versions/090_wecom_category_id.py
+++ backend/alembic/versions/090_wecom_category_id.py
@@ -8,8 +8,8 @@ Create Date: 2026-07-29
 from alembic import op
 import sqlalchemy as sa
 
-revision = "090"
-down_revision = "089"
+revision = "090_wecom_category_id"
+down_revision = "089_wecom_sync_logs"
 branch_labels = None
 depends_on = None
 
